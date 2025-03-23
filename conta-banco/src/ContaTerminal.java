c788a86333bbcc7a4a43d4ce8864b4c79d96ba1d
hugo araujo <hugoaraujope1@gmail.com>
2025-03-23 15:05:49 -0300

    entrega projeto conta

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu número de conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite sua agência:");
+        String agencia = scanner.next();
+
+       
+        scanner.nextLine();
+
+        System.out.println("Digite seu nome completo:");
+        String nome = scanner.nextLine(); 
+
+        System.out.println("Digite seu saldo:");
+        double saldo = scanner.nextDouble();
+
+       
+        System.out.println("Olá, " + nome + ", obrigado por criar conta em nosso banco. Sua agência é: " + agencia + ", sua conta é: " + numeroConta + " e seu saldo é: " + saldo + ". Já está disponível para saque.");
       
-        System.out.println("Helloo, World!");
+        scanner.close();
     }
 }
